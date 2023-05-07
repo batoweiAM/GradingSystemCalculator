7ffe99cda22a4dfb6dadafc82d12ffcf5b632107
Batowei Arepua Michael <113602826+batoweiAM@users.noreply.github.com>
2023-05-07 23:01:40 +0100

    Add project files.

---
+++ GradingSystemCalculator/PrintTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingSystemCalculator
+{
+    class PrintTable
+    {
+        // Field
+        string result;
+        double totalCourseUnit, totalCoursePassed, totalWeightPt, gpa;
+
+        // Constructor
+        public  PrintTable(string result, double totalCourseUnit, double totalCoursePassed, double totalWeightPt, double gpa) 
+        {
+            this.result = result;
+            this.totalCoursePassed = totalCoursePassed;
+            this.totalCourseUnit = totalCourseUnit;
+            this.totalWeightPt = totalWeightPt;
+            this.gpa = totalWeightPt / totalCourseUnit;
+        }
+
+        
+        public void DisplayTable()
+        {
+
+            //Table showing results
+            Console.WriteLine(" TABLE SHOWING STUDENT RESULTS ");
+            Console.WriteLine(" |---------------|-------------|-------|------------|------------|---------------|");
+            Console.WriteLine(" | COURSE & CODE | COURSE UNIT | GRADE | GRADE-UNIT | WEIGHT Pt. | REMARK        |");
+            Console.WriteLine(" |---------------|-------------|-------|------------|------------|---------------|");
+            Console.WriteLine(result);
+            Console.WriteLine(" |_______________|_____________|_______|____________|____________|_______________|");
+            Console.WriteLine();Console.WriteLine();
+            Console.WriteLine("Total Course Unit Registered is: " + totalCourseUnit);
+            Console.WriteLine("Total Course Unit Passed is: " + totalCoursePassed);
+            Console.WriteLine("Total Weight Point is: " + totalWeightPt);
+            Console.WriteLine($"Your GPA is:  {gpa:F2}");
+        }
+    }
+}
---
+++ GradingSystemCalculator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace GradingSystemCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable declaration
+            int InputCount;
+            string CourseCode;
+            int CourseUnit;
+            string Grade;
+            int GradeUnit;
+            int WeightPt;
+            int Score;
+            string Remark;
+            string result = "";
+            double totalWeightPt = 0;
+            double totalCoursePassed = 0;
+            double totalCourseUnit = 0;
+            double gpa = 0;
+
+            Console.WriteLine("GRADE POINT AVERAGE (GPA) CALCULATOR");
+            //Prompting user to input courses offered
+            Console.WriteLine("How many courses do you offer: ");
+            InputCount = Convert.ToInt32(Console.ReadLine());
+
+            //looping through the variables
+            for (int i = 1; i <= InputCount; i++)
+            {
+                Console.WriteLine("What is your course title & code: ");
+                CourseCode = Console.ReadLine();
+                Console.WriteLine("Input course Unit: ");
+                CourseUnit = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input Score: ");
+                Score = Convert.ToInt32(Console.ReadLine());
+
+                //if statement that determines the grade 
+                Grade = Score >= 70 ? "A" : Score >= 60 ? "B" : Score >= 50 ? "C" : Score >= 45 ? "D" : Score >= 40 ? "E" : "F";
+                GradeUnit = Score >= 70 ? 5 : Score >= 60 ? 4 : Score >= 50 ? 3 : Score >= 45 ? 2 : Score >= 40 ? 1 : 0;
+                Remark = GradeUnit == 5 ? "Excellent" : GradeUnit == 4 ? "Very Good" : GradeUnit == 3 ? "Good" : GradeUnit == 2 ? "Fair" : GradeUnit == 1 ? "Pass" : "Fail";
+
+
+                //Calculating weight point
+                WeightPt = (CourseUnit) * (GradeUnit);
+                totalWeightPt += WeightPt;
+                totalCourseUnit += CourseUnit;
+
+                //if statement to exclude failed courses
+                if (GradeUnit != 0)
+                {
+                    totalCoursePassed += CourseUnit;
+                }
+                //Arranging results on the table
+                result = result + $" | {CourseCode,-13} |{CourseUnit,-12} |{Grade,-7}| {GradeUnit,-11}| {WeightPt,-10} |{Remark,-15}|\n";
+
+                //Console.Clear();
+            }
+            PrintTable display = new PrintTable(result, totalCourseUnit, totalCoursePassed, totalWeightPt, gpa);
+            Console.WriteLine(); Console.WriteLine() ; Console.WriteLine() ;
+            display.DisplayTable();
+
+        }
+    }
+}
